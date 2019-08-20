bebf6d1b97569db485df3dc93f5a67ae08f5e423
Vinicius Marzall Lippel <viniciusmlippel@gmail.com>
2019-08-20 18:30:58 -0300

    Exercícios POO1

--- POO1/src/lista4_Matriz/L4_ex17.java
+++ POO1/src/lista4_Matriz/L4_ex17.java
@@ -1,10 +1,91 @@
 package lista4_Matriz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class L4_ex17 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		ArrayList<String> nome = new ArrayList<String>(), telefone = new ArrayList<String>(),
+		celular = new ArrayList<String>(), email = new ArrayList<String>(), dataNasc = new ArrayList<String>(),
+		aniverMes = new ArrayList<String>(), dataAniverMes = new ArrayList<String>();
+		int menu=0, consulta=0, indice, mesCons;
+		String nomeCons;
+		
+		while (menu!=3) {
+			System.out.println("\nMENU \n1- Adicionar contato. \n2- Consultar Contatos. \n3- Sair");
+			menu = entrada.nextInt();
+			entrada.nextLine();
+			
+			if (menu==1) {
+				System.out.print("\nNome: ");
+				nome.add(entrada.nextLine());
+				System.out.print("Telefone: ");
+				telefone.add(entrada.nextLine());
+				System.out.print("Celular: ");
+				celular.add(entrada.nextLine());
+				System.out.print("E-mail: ");
+				email.add(entrada.nextLine());
+				System.out.print("Data de nascimento (DD/MM/AAAA): ");
+				dataNasc.add(entrada.nextLine());
+			
+			}
+			
+			else if (menu==2) {
+				System.out.println("\n1- Consulta por nome \n2- Consulta por mês de aniversário ");
+				consulta = entrada.nextInt();
+				entrada.nextLine();
+				
+				if (consulta==1) {
+					indice = -1;
+					System.out.print("\nNome: ");
+					nomeCons = entrada.nextLine();
+					
+					int i=0;
+					while (i<nome.size() && indice==-1) {
+						if (nomeCons.equalsIgnoreCase(nome.get(i)))
+							indice = i;
+						i++;
+					}
+					
+					if (indice != -1) {
+						System.out.println("\nNome: "+nome.get(indice));
+						System.out.println("Telefone: "+telefone.get(indice));
+						System.out.println("Celular: "+celular.get(indice));
+						System.out.println("E-mail: "+email.get(indice));
+						System.out.println("Data de nascimento: "+dataNasc.get(indice));
+					}
+					else
+						System.out.println("Contato inexistente");
+					
+				}
+				
+				else if (consulta==2) {
+					indice = -1;
+					System.out.println("\nMes: ");
+					mesCons = entrada.nextInt();
+				
+					for (int i=0; i<dataNasc.size(); i++) {
+						if (mesCons == Integer.parseInt(dataNasc.get(i).substring(3,5))) {
+							aniverMes.add(nome.get(i));
+							dataAniverMes.add(dataNasc.get(i));
+						}
+					}
+					
+					System.out.println("\nAniversariantes do mês "+mesCons+": ");
+					for (int i=0;i<aniverMes.size(); i++) {
+						System.out.println("Nome: "+aniverMes.get(i)+" / "
+								+ "Data de nascimento: "+dataAniverMes.get(i));
+					}
+				}
+			}
+				
+			
+		}
+		
+		entrada.close();
 	}
 
 }
--- POO1/src/lista4_Matriz/L4_ex3.java
+++ POO1/src/lista4_Matriz/L4_ex3.java
@@ -7,16 +7,52 @@ public class L4_ex3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner entrada = new Scanner(System.in);
-		int[] vetor = new int[10];
+		final int n = 10;
+		int soma=0, maior=0, menor=0, media;
+		int[] vetor = new int[n];
 		
 		System.out.println("Valores: ");
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<n; i++) {
 			System.out.print("-> ");
 			vetor[i] = entrada.nextInt();
+			
+			if (i==0)
+				menor = vetor[i];
+			
+			if (vetor[i]>maior)
+				maior = vetor[i];
+			else if (vetor[i]<menor)
+				menor = vetor[i];
+			
+			soma = soma+vetor[i];
 		}
 		
-		for (int i=9; i>=0; i--)
-			System.out.print(vetor[i]+"; ");
+		media  = soma/n;
+		
+		System.out.println("Soma: "+soma);
+		System.out.println("Média: "+media);
+		System.out.println("Maior: "+maior);
+		System.out.println("Menor: "+menor);
+		System.out.print("Pares: ");
+		for (int i=0; i<n; i++) {
+			if (vetor[i]%2==0)
+				System.out.print(vetor[i]+"; ");
+		}
+		System.out.print("\nImpares: ");
+		for (int i=0; i<n; i++) {
+			if (vetor[i]%2!=0)
+				System.out.print(vetor[i]+"; ");
+		}
+		System.out.print("\nAcima da média: ");
+		for (int i=0; i<n; i++) {
+			if (vetor[i]>media)
+				System.out.print(vetor[i]+"; ");
+		}
+		System.out.print("\nAbaixo da media: ");
+		for (int i=0; i<n; i++) {
+			if (vetor[i]<media)
+				System.out.print(vetor[i]+"; ");
+		}
 		
 		entrada.close();
 	}
